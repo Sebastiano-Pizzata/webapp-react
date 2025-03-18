ba2cae02a42ddddd9b2a281a907303964b134af1
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-03-18 17:29:00 +0100

    ADD: HomePage and MoviesCard

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+export default function MovieCard({ movie }) {
+    const { id, title, author, abstract, image } = movie;
+
+    return (
+        <>
+            <div className='card mb-4'>
+                <img src={image} alt={title} />
+                <div className='card-body mb-4'>
+                    <h5 className='card-title'>{title}</h5>
+                    <span>{author}</span>
+                    <p>{abstract}</p>
+                    <Link to={`movies/${id}`}>Read More</Link>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import MovieCard from '../components/MovieCard';
+
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+
+    const url = import.meta.env.VITE_ENDPOINT_URL;
+
+    const fetchMovie = () => {
+        axios.get(url)
+            .then(res => setMovies(res.data))
+            .catch((error) => { console.log(error) })
+    }
+
+
+    const renderMovies = () => {
+        return movies.map((movie) => {
+            return (
+                <div className='col' key={movie.id}>
+                    <MovieCard movie={movie} />
+                </div>
+            )
+        })
+    }
+
+    useEffect(fetchMovie, []);
+
+
     return (
         <>
             <h1 className="text-primary">Movies</h1>
             <h2>Qui andranno i film</h2>
-            <div className="row row-cols-3"></div>
+            <div className="row row-cols-3">
+                {renderMovies()}
+            </div>
         </>
     )
 }
