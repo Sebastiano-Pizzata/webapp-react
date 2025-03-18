7cbe85c6fc20fc57a887f21bad510d23dd51c3e8
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-03-18 18:01:59 +0100

    ADD: ReviewCard and MoviePage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,5 +1,45 @@
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+import ReviewCard from '../components/ReviewCard'
+
+
 export default function MoviePage() {
+
+    const { id } = useParams();
+    const [movie, setMovie] = useState({});
+
+    const url = `${import.meta.env.VITE_ENDPOINT_URL}/${id}`;
+
+    const fetchMovie = () => {
+        axios.get(url)
+            .then(res => {
+                console.log(res.data);
+                setMovie(res.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    useEffect(fetchMovie, [id])
+
+    const renderReview = () => {
+        return movie.reviews?.map((review) => {
+            return <ReviewCard key={review.id} review={review} />
+        })
+    }
+
+
     return (
-        <h1 className="text-primary">Singolo Movie</h1>
+        <>
+            <h1 className="text-primary">{movie.title}</h1>
+            <img src={movie.image} alt={movie.title} />
+            <section>
+                <h4>Recensioni della Community</h4>
+                {renderReview()}
+            </section>
+        </>
     )
 }
